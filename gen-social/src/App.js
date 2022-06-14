efddca065fb8792447889a0805ef030404cfbc0c
Elyriand21 <felix.banks02@gmail.com>
2022-06-13 20:23:55 -0400

    Please read this file.

--- gen-social/src/App.js
+++ gen-social/src/App.js
@@ -3,6 +3,9 @@ import Login from "../src/Components/Login/Login"
 import LoginHeader from "./Components/Login/LoginHeader"
 import { BrowserRouter } from "react-router-dom";
 
+// THIS FILE IS NOT NEEDED. PLEASE DELETE THIS FILE FROM THE REPOSITORY
+// IT IS JUST TAKING UP SPACE LMAO
+
 function App() {
   return (
     <BrowserRouter>
