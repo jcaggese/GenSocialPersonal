612ee9a57ba9c80c97344bbea10fbf3c16e7704d
Elyriand21 <felix.banks02@gmail.com>
2022-06-16 12:12:42 -0400

    Minor cosmetics and functionality changes

--- gen-social/src/Components/Settings/Settings.jsx
+++ gen-social/src/Components/Settings/Settings.jsx
@@ -6,20 +6,30 @@ import DeletePopUp from "./DeletePopUp"
 import bcrypt from 'bcryptjs'
 
 const Settings = () => {
-
+    // Variables
+    // Variables for what decision is made
     const [decision, setDecision] = useState(0);
+    // Variables to store the new username
     const [newUsername, setNewUsername] = useState('');
+    // Variables to store the current username
     const [currentUsername, setCurrentUsername] = useState('');
+    // Variables to store the typed password
     const [password, setPassword] = useState('');
+    // Variables to store the typed email
     const [email, setEmail] = useState('');
+    // Variables to store the current email
     const [currentEmail, setCurrentEmail] = useState('');
+    // Variables to store the new email
     const [newEmail, setNewEmail] = useState('');
+    // Variables to store whethet popup is open or not
     const [isOpen, setIsOpen] = useState(false);
+    // Variables for the error messages
     const [err, setError] = useState(0);
+    // Variables for success messages
     const [success, setSuccess] = useState(0);
 
+    // Navigation
     const history = useNavigate();
-
     const togglePopUp = () => {
         setIsOpen(!isOpen);
     }
@@ -46,7 +56,46 @@ const Settings = () => {
             email
         )
     }
+    // Validate the fields
+    const validate = (e) => {
+        var hashedPassword;
+        var passwordsMatch;
+        if (e.target.id === 'changeUserBtn' || e.target.className === "new-username-input") {
+            if (currentUsername !== localStorage.getItem("loggedUser")) {
+                setError(1)
+            }
+            else {
+                setError(0)
+                console.log("works");
+                updateUsername();
+            }
+        }
+        else {
+            axios.post(`http://localhost:9080/users/`, {
+                username: currentUsername
+            }).then((res) => {
+                hashedPassword = bcrypt.hashSync(password, res.data.salt)
 
+                passwordsMatch = hashedPassword === "$" + res.data.password
+                // Username and Password is correct
+                if (passwordsMatch) {
+                    console.log("Correct password");
+                    setError(0)
+                    togglePopUp();
+                }
+                // Password is incorrect
+                else {
+                    console.log("Passwords don't match");
+                    setError(2)
+                }
+            }).catch(err => {
+                if (err.response.status === 404) {
+                    console.log("Username incorrect");
+                    setError(1)
+                }
+            })
+        }
+    }
     // Function to change the username in the server
     const updateUsername = () => {
         console.log(currentUsername);
@@ -85,18 +134,6 @@ const Settings = () => {
             }
         }
     }
-
-    // Function to handle which key is pressed
-    const handleKeyPress = e => {
-        console.log(e.target.className);
-        if (e.keyCode === 13 && e.target.className === "new-username-input") {
-            validate(e);
-        }
-        if (e.keyCode === 13 && e.target.className === "new-email-input") {
-            validate();
-        }
-    }
-
     // Function to handle updating the email
     const updateEmail = async () => {
         console.log(currentEmail);
@@ -105,16 +142,22 @@ const Settings = () => {
 
         if (newEmail === '') {
             console.log("New email field is blank ");
+            setSuccess(0)
+            setError(3)
             return;
         }
 
         if (currentEmail !== localStorage.getItem("loggedEmail")) {
             console.log("Current email does not match the email on file");
+            setSuccess(0)
+            setError(4)
         }
         else {
             console.log("Current email matches the email on file");
             if (newEmail === currentEmail) {
                 console.log("New email matches current email");
+                setSuccess(0)
+                setError(5)
             }
             else {
                 console.log("New email does NOT match current email");
@@ -132,75 +175,11 @@ const Settings = () => {
                         // If the email didn't update
                         else {
                             console.log("Oopsies! There was an error!");
+                            setSuccess(0)
+                            setError(6)
                         }
                     })
             }
-        }
-    }
-
-    // Function to handle the new username
-    const handleNewUsername = (e) => {
-        setNewUsername(e.target.value)
-    }
-    // Function to handle the current username
-    const handleCurrentUsername = (e) => {
-        setCurrentUsername(e.target.value)
-    }
-    // Function to handle current email
-    const handleCurrentEmail = (e) => {
-        setCurrentEmail(e.target.value)
-    }
-    // Function to handle new email
-    const handleNewEmail = (e) => {
-        setNewEmail(e.target.value)
-    }
-    // Function to handle password
-    const handlePassword = (e) => {
-        setPassword(e.target.value);
-    }
-    // Switch the browser to the login page
-    const goToHome = async () => {
-        await deleteAccount();
-        history('/');
-    }
-    // Validate the fields
-    const validate = (e) => {
-        var hashedPassword;
-        var passwordsMatch;
-        if (e.target.id === 'changeUserBtn' || e.target.className === "new-username-input") {
-            if (currentUsername !== localStorage.getItem("loggedUser")) {
-                setError(1)
-            }
-            else{
-                setError(0)
-                console.log("works");
-                updateUsername();
-            }
-        }
-        else {
-            axios.post(`http://localhost:9080/users/`, {
-                username: currentUsername
-            }).then((res) => {
-                hashedPassword = bcrypt.hashSync(password, res.data.salt)
-
-                passwordsMatch = hashedPassword === "$" + res.data.password
-                // Username and Password is correct
-                if (passwordsMatch) {
-                    console.log("Correct password");
-                    setError(0)
-                    togglePopUp();
-                }
-                // Password is incorrect
-                else {
-                    console.log("Passwords don't match");
-                    setError(2)
-                }
-            }).catch(err => {
-                if (err.response.status === 404) {
-                    console.log("Username incorrect");
-                    setError(1)
-                }
-            })
         }
     }
     // Function to delete the account
@@ -251,7 +230,7 @@ const Settings = () => {
                     if (err.response.status === 404) {
                         console.log("User not found")
                     }
-                    else{
+                    else {
                         console.log("error");
                     }
                 });
@@ -259,34 +238,100 @@ const Settings = () => {
             }
         }
     }
+
+    // Function to handle which key is pressed
+    const handleKeyPress = e => {
+        console.log(e.target.className);
+        if (e.keyCode === 13 && e.target.className === "new-username-input") {
+            validate(e);
+        }
+        if (e.keyCode === 13 && e.target.className === "new-email-input") {
+            validate();
+        }
+    }
+    // Function to handle the new username
+    const handleNewUsername = (e) => {
+        setNewUsername(e.target.value)
+    }
+    // Function to handle the current username
+    const handleCurrentUsername = (e) => {
+        setCurrentUsername(e.target.value)
+    }
+    // Function to handle current email
+    const handleCurrentEmail = (e) => {
+        setCurrentEmail(e.target.value)
+    }
+    // Function to handle new email
+    const handleNewEmail = (e) => {
+        setNewEmail(e.target.value)
+    }
+    // Function to handle password
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+    // Switch the browser to the login page
+    const goToHome = async () => {
+        await deleteAccount();
+        history('/');
+    }
+
     // Function to display error messages
     const errorMessage = () => {
+        // Shows that the username is incorrect
         if (err === 1) {
             return (
                 <small style={{ display: err ? '' : 'none', color: "black" }}>Username is incorrect</small>
             );
         }
+        // Shows that the password is incorrect
         if (err === 2) {
             return (
                 <small style={{ display: err ? '' : 'none', color: "black" }}>Password is incorrect</small>
             );
         }
+        // The new email field is empty
+        if (err === 3) {
+            return (
+                <small style={{ display: err ? '' : 'none', color: "black" }}>New email is empty</small>
+            );
+        }
+        // Current email doesn't match database
+        if (err === 4) {
+            return (
+                <small style={{ display: err ? '' : 'none', color: "black" }}>Current email does not match the one on file</small>
+            );
+        }
+        // New email is the same as current email
+        if (err === 5) {
+            return (
+                <small style={{ display: err ? '' : 'none', color: "black" }}>New email matches current email</small>
+            );
+        }
+        // If none of these errors happen, I don't really know what even went on. Congratulations, you broke it.
+        if (err === 6) {
+            return (
+                <small style={{ display: err ? '' : 'none', color: "black" }}>Honestly, I don't even know what happened. Try again</small>
+            );
+        }
     }
     // Function to display success messages
     const successMessage = () => {
-        if(success === 1){
-            return(
+        // Username is successfully updates
+        if (success === 1) {
+            return (
                 <h1>Username successfully updated!</h1>
             )
         }
-        if(success === 2){
-            return(
+        // Email is successfully updated
+        if (success === 2) {
+            return (
                 <h1>Email successfully updated!</h1>
             )
         }
     }
     // Show the settings page
     const showSettingsMain = () => {
+        // If the decision is to see the main setting screen
         if (decision === 0) {
             return (
                 <div>
@@ -377,7 +422,7 @@ const Settings = () => {
                                     <input onChange={handleNewUsername} onKeyDown={handleKeyPress} style={{ marginLeft: "10px" }} type="text" className='new-username-input' placeholder='Please type desired username here'></input>
                                 </div>
                                 <small> {errorMessage()}</small>
-                                <div> {successMessage()}</div>
+                                <small>{successMessage()}</small>
                                 <div className='change-username-div'>
                                     <div>
                                         <button style={{ backgroundColor: "red", margin: "10px" }} id="changeUserBtn" onClick={validate} className='btn btn-primary mb-1'>Change Username</button>
@@ -418,6 +463,8 @@ const Settings = () => {
                                         <input style={{ marginLeft: "10px", width: "265px" }} type="password" className='confirm-password-input' placeholder='Please type desired password here'></input>
                                     </div>
                                 </div>
+                                <small> {errorMessage()}</small>
+                                <small> {successMessage()}</small>
                                 <div className='change-username-div'>
                                     <div>
                                         <button type="submit" style={{ backgroundColor: "red" }} className='btn btn-primary mb-1'>Submit</button>
@@ -454,7 +501,8 @@ const Settings = () => {
                                         <input onChange={handleNewEmail} onKeyDown={handleKeyPress} style={{ marginLeft: "10px", width: "210px" }} type="text" className='new-email-input' placeholder='Please type new email here'></input>
                                     </div>
                                 </div>
-                                {successMessage()}
+                                <small> {errorMessage()}</small>
+                                <small> {successMessage()}</small>
                                 <div className='change-username-div'>
                                     <div>
                                         <button type="change-email" onClick={updateEmail} style={{ backgroundColor: "red" }} className='btn btn-primary mb-1'>Submit</button>
